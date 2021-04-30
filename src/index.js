d6c16a8045e0881cf01f82a76c83df755d5f3895
fredyflemus <fredyflemus@gmail.com>
2021-04-30 17:20:37 -0600

    first commit

---
+++ src/index.js
@@ -0,0 +1,18 @@
+const messages = [
+    "fredy",
+    "Lucia",
+    "Omar",
+    "Luis",
+    "Jessica",
+    "Diego",
+    "Laura"
+];
+
+const randomMsg = () => {
+    const message = messages[Math.floor(Math.random() * messages.length)];
+    console.log(message);
+};
+
+module.exports = {
+    randomMsg
+};
